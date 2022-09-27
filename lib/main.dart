accf581e42ddd844608d89ac4a2ff1800fece2f3
faizanShoukat55 <faizanshoukat45@gmail.com>
2022-09-27 21:05:59 +0500

    Jazz Cash API Implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -24,92 +29,114 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Jazz Cash Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+
+  payment() async {
+    var digest;
+    String dateandtime = DateFormat("yyyyMMddHHmmss").format(DateTime.now());
+    String dexpiredate = DateFormat("yyyyMMddHHmmss").format(
+        DateTime.now().add(Duration(days: 1)));
+    String tre = "T" + dateandtime;
+    String pp_Amount = "100000";
+    String pp_BillReference = "billRef";
+    String pp_Description = "Description";
+    String pp_Language = "EN";
+    /// confidential data
+    String pp_MerchantID = "Your Merchant Id";
+    String pp_Password = "Your Password";
+    String IntegeritySalt = "Your Integerity Salt";
+
+    String pp_ReturnURL = "https://sandbox.jazzcash.com.pk/ApplicationAPI/API/Payment/DoTransaction";
+    String pp_ver = "1.1";
+    String pp_TxnCurrency = "PKR";
+    String pp_TxnDateTime = dateandtime.toString();
+    String pp_TxnExpiryDateTime = dexpiredate.toString();
+    String pp_TxnRefNo = tre.toString();
+    String pp_TxnType = "MWALLET";
+    String ppmpf_1 = "4456733833993";
+
+    String and = '&';
+    String superdata =
+        IntegeritySalt + and +
+            pp_Amount + and +
+            pp_BillReference + and +
+            pp_Description + and +
+            pp_Language + and +
+            pp_MerchantID + and +
+            pp_Password + and +
+            pp_ReturnURL + and +
+            pp_TxnCurrency + and +
+            pp_TxnDateTime + and +
+            pp_TxnExpiryDateTime + and +
+            pp_TxnRefNo + and +
+            pp_TxnType + and +
+            pp_ver + and +
+            ppmpf_1
+    ;
+
+
+    var key = utf8.encode(IntegeritySalt);
+    var bytes = utf8.encode(superdata);
+    var hmacSha256 = new Hmac(sha256, key);
+    Digest sha256Result = hmacSha256.convert(bytes);
+    var url = 'https://sandbox.jazzcash.com.pk/ApplicationAPI/API/Payment/DoTransaction';
+
+    var response = await http.post(Uri.parse(url), body: {
+      "pp_Version": pp_ver,
+      "pp_TxnType": pp_TxnType,
+      "pp_Language": pp_Language,
+      "pp_MerchantID": pp_MerchantID,
+      "pp_Password": pp_Password,
+      "pp_TxnRefNo": tre,
+      "pp_Amount": pp_Amount,
+      "pp_TxnCurrency": pp_TxnCurrency,
+      "pp_TxnDateTime": dateandtime,
+      "pp_BillReference": pp_BillReference,
+      "pp_Description": pp_Description,
+      "pp_TxnExpiryDateTime": dexpiredate,
+      "pp_ReturnURL": pp_ReturnURL,
+      "pp_SecureHash": sha256Result.toString(),
+      "ppmpf_1": "4456733833993"
     });
+
+    print(response.statusCode);
+    print("response=>");
+    print(response.body);
   }
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: Container(
+          child: Center(
+            child: ElevatedButton(
+                onPressed: () {
+                  payment();
+                }, child: Text("Pay with JazzCash"),
+
+            ),
+
+          ),
+        )
+
     );
   }
 }
